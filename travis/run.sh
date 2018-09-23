154d7c94f239ddc6fa517e3a648b21e7e1363d33
Wiktor Niesiobedzki <github@vink.pl>
2018-09-23 13:59:16 +0200

    Use JDK8 and 10. Simplify things

--- travis/run.sh
+++ travis/run.sh
@@ -2,27 +2,6 @@
 
 die() { echo "$@" 1>&2 ; exit 1; }
 
-path="$HOME/travis"
-mkdir -p "$path"
-if ! [ -d "$path/apache-ant-1.9.7" ]; then
-	wget -qO- http://www-eu.apache.org/dist//ant/binaries/apache-ant-1.10.5-bin.tar.gz | tar xz -C "$path" || die "Could not install ANT."
-fi
-ANT=$HOME/travis/apache-ant-1.9.7
-
-if [ "$JDK" = "oraclejdk9" ]; then
-	if ! [ -d "$path/jdk-9" ]; then
-		V=128
-		wget -qO- http://www.java.net/download/java/jdk9/archive/$V/binaries/jdk-9-ea+${V}_linux-x64_bin.tar.gz | tar xz -C "$path" || die "Could not install JDK."
-	fi
-	export JAVA_HOME="$path/jdk-9"
-	export PATH="$JAVA_HOME/bin:$PATH"
-elif [ "$JDK" = "oraclejdk8" ]; then
-	# Default
-	true
-else
-	 die "could not find JDK".
-fi
-
 echo "java -version:"
 java -Xmx32m -version
 echo "javac -version:"
@@ -31,4 +10,4 @@ echo
 
 echo "Using osm dev user $OSM_USERDEF"
 
-ANT_OPTS="-Xmx600m" $ANT/bin/ant $OSM_USERDEF $OSM_PASSWORDDEF $TARGET
+ANT_OPTS="-Xmx600m" ant $OSM_USERDEF $OSM_PASSWORDDEF $TARGET
